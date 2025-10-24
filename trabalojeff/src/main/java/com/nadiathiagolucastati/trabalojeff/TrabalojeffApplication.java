25cade61d9f66ef4347d04fca4f2abb78674a0f0
nadiaszk <nadia.szk@gmail.com>
2025-10-23 21:39:10 -0300

    Criacao do projeto trabalojeff

---
+++ trabalojeff/src/main/java/com/nadiathiagolucastati/trabalojeff/TrabalojeffApplication.java
@@ -0,0 +1,13 @@
+package com.nadiathiagolucastati.trabalojeff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalojeffApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrabalojeffApplication.class, args);
+    }
+
+}
---
+++ trabalojeff/src/test/java/com/nadiathiagolucastati/trabalojeff/TrabalojeffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nadiathiagolucastati.trabalojeff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalojeffApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
