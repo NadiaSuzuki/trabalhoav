1a96a605e3f9cacf565a1fcdf1111374ac1638ee
nadiaszk <nadia.szk@gmail.com>
2025-12-04 08:39:15 -0300

    Delete gestaofinanceirapessoal-main/gestaofinanceirapessoal-main directory

--- gestaofinanceirapessoal-main/gestaofinanceirapessoal-main/README.md
+++
@@ -1,3 +0,0 @@
-# gestaofinanceirapessoal
-# gestaofinanceirapessoal
-# gestaofinanceirapessoal
--- gestaofinanceirapessoal-main/gestaofinanceirapessoal-main/src/main/java/com/gestaofinanceirapessoal/repositories/TransacaoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.gestaofinanceirapessoal.repositories;
-
-import com.gestaofinanceirapessoal.domains.Transacao;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TransacaoRepository extends JpaRepository<Transacao, Long> {
-}
